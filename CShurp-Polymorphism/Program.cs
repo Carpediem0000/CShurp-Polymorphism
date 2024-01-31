4c36f2c54fde6626220db62c63a71d0a3bd80e5d
CarpeDiem0000 <zinovchukdfuz2@gmail.com>
2024-01-31 16:40:39 +0200

    Add project files.

---
+++ CShurp-Polymorphism/Program.cs
@@ -0,0 +1,41 @@
+﻿using Elf;
+namespace CShurp_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElvenMystic elvenWizard = new ElvenWizard("ElvenWizard", "Posoh", 5);
+            elvenWizard.Atack();
+            elvenWizard.Voice();
+
+            ElvenMystic eleventalSummoner = new EleventalSummoner("EleventalSummoner", "Posoh", 15);
+            eleventalSummoner.Atack();
+            eleventalSummoner.Voice();
+
+            ElvenMystic eleventalMaster = new EleventalMaster("EleventalMaster", "Posoh", 45);
+            eleventalMaster.Atack();
+            eleventalMaster.Voice();
+
+            ElvenMystic spellsinger = new Spellsinger("Spellsinger", "Posoh", 1);
+            spellsinger.Atack();
+            spellsinger.Voice();
+
+            ElvenMystic mysticMuse = new MysticMuse("MysticMuse", "Posoh", 7);
+            mysticMuse.Atack();
+            mysticMuse.Voice();
+
+            ElvenMystic elvenOracle = new ElvenOracle("ElvenOracle", "Posoh", 100);
+            elvenOracle.Atack();
+            elvenOracle.Voice();
+
+            ElvenMystic elvenElder = new ElvenElder("ElvenElder", "Posoh", 32);
+            elvenElder.Atack();
+            elvenElder.Voice();
+
+            ElvenMystic euasSaint = new EuasSaint("EuasSaint", "Posoh", 61);
+            euasSaint.Atack();
+            euasSaint.Voice();
+        }
+    }
+}
---
+++ Elf/Class1.cs
@@ -0,0 +1,138 @@
+﻿namespace Elf
+{
+    public abstract class Elf
+    {
+        public string Name;
+    }
+
+    public abstract class ElvenMystic : Elf
+    {
+        public string Weapon;
+        public int Health;
+        public int Attack;
+        public int Lvl;
+        virtual public void Atack() { }
+        virtual public void Voice() { }
+    }
+
+    public class ElvenWizard : ElvenMystic
+    {
+        public ElvenWizard(string name, string weapon, int lvl)
+        {
+            Name = name;
+            Weapon = weapon;
+            Health = lvl*500;
+            Attack = lvl*5;
+            Lvl = lvl;
+        }
+        public override void Atack()
+        {
+            Console.WriteLine("ElvenWizard atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("ElvenWizard voice");
+        }
+    }
+    public class EleventalSummoner : ElvenWizard
+    {
+        public EleventalSummoner(string name, string weapon, int lvl) : base(name, weapon, lvl) { }
+        public override void Atack()
+        {
+            Console.WriteLine("EleventalSummoner atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("EleventalSummoner voice");
+        }
+    }
+    public class EleventalMaster : EleventalSummoner
+    {
+        public EleventalMaster(string name, string weapon, int lvl) : base(name, weapon, lvl) { }
+        public override void Atack()
+        {
+            Console.WriteLine("EleventalMaster atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("EleventalMaster voice");
+        }
+    }
+
+    public class Spellsinger : ElvenWizard
+    {
+        public Spellsinger(string name, string weapon, int lvl) : base(name, weapon, lvl) { }
+        public override void Atack()
+        {
+            Console.WriteLine("Spellsinger atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("Spellsinger voice");
+        }
+    }
+    public class MysticMuse : Spellsinger
+    {
+        public MysticMuse(string name, string weapon, int lvl) : base(name, weapon, lvl) { }
+        public override void Atack()
+        {
+            Console.WriteLine("MysticMuse atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("MysticMuse voice");
+        }
+    }
+
+    public class ElvenOracle : ElvenMystic
+    {
+        public ElvenOracle(string name, string weapon, int lvl)
+        {
+            Name = name;
+            Weapon = weapon;
+            Health = lvl * 350;
+            Attack = lvl * 3;
+            Lvl = lvl;
+        }
+        public override void Atack()
+        {
+            Console.WriteLine("ElvenOracle atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("ElvenOracle voice");
+        }
+    }
+    public class ElvenElder : ElvenOracle
+    {
+        public ElvenElder(string name, string weapon, int lvl) : base(name, weapon, lvl) { }
+        public override void Atack()
+        {
+            Console.WriteLine("ElvenElder atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("ElvenElder voice");
+        }
+    }
+    public class EuasSaint : ElvenElder
+    {
+        public EuasSaint(string name, string weapon, int lvl) : base(name, weapon, lvl) { }
+        public override void Atack()
+        {
+            Console.WriteLine("EuasSaint atack");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("EuasSaint voice");
+        }
+    }
+}
